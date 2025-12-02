1425c0cba73917dee17101dc7bb79c1951c224a8
Patrick Himmelmann <himmelmannp@gmail.com>
2025-12-03 00:24:59 +0100

    Day 2 part 2 brute force

--- src/aoc2025/d02.rs
+++ src/aoc2025/d02.rs
@@ -9,6 +9,30 @@ fn is_invalid1(n: u64) -> bool {
     let mid = b.len() / 2;
     b[..mid] == b[mid..]
 }
+
+fn is_invalid2(n: u64) -> bool {
+    let s = n.to_string();
+    let b = s.as_bytes();
+    'outer: for l in 1..=b.len() / 2 {
+        let chunk = b.chunks_exact(l);
+        if !chunk.remainder().is_empty() {
+            continue;
+        }
+        let mut val = None;
+        for c in chunk {
+            if let Some(v) = val {
+                if v != c {
+                    continue 'outer;
+                }
+            } else {
+                val = Some(c);
+            }
+        }
+        return true;
+    }
+    false
+}
+
 pub fn f(input: AocInput) -> crate::AocResult {
     let mut res1 = 0;
     let mut res2 = 0;
@@ -20,6 +44,9 @@ pub fn f(input: AocInput) -> crate::AocResult {
         for i in start..=end {
             if is_invalid1(i) {
                 res1 += i;
+            }
+            if is_invalid2(i) {
+                res2 += i;
             }
         }
     }
