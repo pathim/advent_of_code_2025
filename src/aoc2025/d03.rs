fc1617edfaef648c66e4539e498b9e472857b019
Patrick Himmelmann <himmelmannp@gmail.com>
2025-12-03 23:23:04 +0100

    day3 part2

--- src/aoc2025/d03.rs
+++ src/aoc2025/d03.rs
@@ -1,25 +1,40 @@
 use crate::AocInput;
 
+fn find_max_first(values: &[u64], start: usize, end: usize) -> (usize, u64) {
+    let mut oldmax = (0, 0);
+    for v in values[start..end].iter().copied().enumerate() {
+        if v.1 > oldmax.1 {
+            oldmax = v;
+        }
+    }
+    oldmax
+}
+
+fn find_joltage(values: &[u64], n:usize) -> u64 {
+    let mut start = 0;
+    let mut result = 0;
+    for i in 0..n {
+        let max = find_max_first(values, start, values.len() - n + i + 1);
+        result *= 10;
+        result += max.1;
+        start += max.0 + 1;
+    }
+    result
+}
+
 pub fn f(input: AocInput) -> crate::AocResult {
     let mut res1 = 0;
+    let mut res2 = 0;
     for line in input.lines() {
         let line = line
             .unwrap()
             .chars()
             .map(|c| (c as u64) - ('0' as u64))
             .collect::<Vec<_>>();
-        let max1 = line.iter().take(line.len() - 1).copied().enumerate().fold(
-            (0usize, 0u64),
-            |oldmax, current| {
-                if current.1 > oldmax.1 {
-                    current
-                } else {
-                    oldmax
-                }
-            },
-        );
-        let max2 = *line.iter().skip(max1.0 + 1).max().unwrap();
-        res1 += max1.1 * 10 + max2;
+
+        res1 += find_joltage(&line, 2);
+        let jolt = find_joltage(&line, 12);
+        res2 += jolt;
     }
-    res1.into()
+    (res1, res2).into()
 }
