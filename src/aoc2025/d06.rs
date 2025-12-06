87be26e7c5a7444e664a8e92d9efe0ce1b434978
Patrick Himmelmann <himmelmannp@gmail.com>
2025-12-07 00:25:21 +0100

    day6 optimization

--- src/aoc2025/d06.rs
+++ src/aoc2025/d06.rs
@@ -22,33 +22,47 @@ impl Operations {
     }
 }
 
+impl TryFrom<char> for Operations {
+    type Error = ();
+
+    fn try_from(value: char) -> Result<Self, Self::Error> {
+        match value {
+            '+' => Ok(Self::Add),
+            '*' => Ok(Self::Mul),
+            _ => Err(()),
+        }
+    }
+}
+
 pub fn f(input: AocInput) -> AocResult {
     let mut numbers = Vec::new();
-    let mut lines = Vec::new();
+    let mut numbers2 = Vec::new();
     let mut operations = Vec::new();
     for l in input.lines().flatten() {
         let mut current_line = Vec::new();
         let mut current_number = None;
-        lines.push(l.chars().collect::<Vec<_>>());
-        for c in l.chars() {
+        for (i, c) in l.chars().enumerate() {
+            if numbers2.len() < i + 1 {
+                numbers2.push(0u64);
+            }
+            let val = numbers2.get_mut(i).unwrap();
+            let num = c.to_digit(10).map(|x| x as u64);
+            if let Some(n) = num {
+                *val = *val * 10 + n;
+            }
             if c.is_whitespace() {
                 if let Some(v) = current_number.take() {
                     current_line.push(v);
                 }
                 continue;
             }
-            if c == '+' {
-                operations.push(Operations::Add);
+            if let Ok(op) = Operations::try_from(c) {
+                operations.push(op);
                 continue;
             }
-            if c == '*' {
-                operations.push(Operations::Mul);
-                continue;
-            }
-            let num = c as u64 - '0' as u64;
             let cur = current_number.get_or_insert(0);
             *cur *= 10;
-            *cur += num;
+            *cur += num.unwrap();
         }
         if let Some(v) = current_number {
             current_line.push(v);
@@ -65,26 +79,10 @@ pub fn f(input: AocInput) -> AocResult {
             .copied()
             .fold(o.init(), |a, b| o.exec(a, b));
     }
-    lines.pop();
-    let mut o_idx = 0;
-    let mut op = &operations[o_idx];
-    let mut val = op.init();
-    let mut res2 = 0;
-    for col in 0..lines[0].len() {
-        let num = lines
-            .iter()
-            .map(|l| l[col])
-            .filter(|c| !c.is_whitespace())
-            .fold(0, |a, b| a * 10 + (b as u64) - ('0' as u64));
-        if num == 0 {
-            res2 += val;
-            o_idx += 1;
-            op = &operations[o_idx];
-            val = op.init();
-        } else {
-            val = op.exec(val, num);
-        }
-    }
-    res2 += val;
+    let res2 = numbers2
+        .split(|a| *a == 0)
+        .zip(operations.iter())
+        .map(|(ns, op)| ns.iter().fold(op.init(), |a, b| op.exec(a, *b)))
+        .sum::<u64>();
     (res1, res2).into()
 }
