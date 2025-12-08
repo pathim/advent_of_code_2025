c3b7be653db0411714e5f573fa1f260cfb5c9664
Patrick Himmelmann <himmelmannp@gmail.com>
2025-12-08 22:37:42 +0100

    Day 8 part1

---
+++ src/aoc2025/d08.rs
@@ -0,0 +1,83 @@
+use std::cmp::Reverse;
+use std::{
+    collections::{BinaryHeap, HashMap, HashSet},
+    str::FromStr,
+};
+
+use crate::{AocInput, AocResult};
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+struct Node {
+    x: i64,
+    y: i64,
+    z: i64,
+}
+
+impl Node {
+    fn dist2(&self, other: &Self) -> i64 {
+        let dx = self.x - other.x;
+        let dy = self.y - other.y;
+        let dz = self.z - other.z;
+        dx * dx + dy * dy + dz * dz
+    }
+}
+
+impl FromStr for Node {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let v = s.split(',').map(|a| a.parse().unwrap()).collect::<Vec<_>>();
+        if v.len() != 3 {
+            Err(())
+        } else {
+            Ok(Self {
+                x: v[0],
+                y: v[1],
+                z: v[2],
+            })
+        }
+    }
+}
+
+pub fn f(input: AocInput) -> AocResult {
+    let nodes = input
+        .lines()
+        .flatten()
+        .map(|l| Node::from_str(&l).unwrap())
+        .collect::<Vec<_>>();
+    let mut node_networks = (0..nodes.len()).enumerate().collect::<HashMap<_, _>>();
+    let mut network_nodes = (0..nodes.len())
+        .map(|i| (i, vec![i]))
+        .collect::<HashMap<_, _>>();
+    let mut dists = BinaryHeap::new();
+    for i in 0..nodes.len() - 1 {
+        for j in i + 1..nodes.len() {
+            dists.push(Reverse((nodes[i].dist2(&nodes[j]), (i, j))));
+        }
+    }
+    let mut res1 = 0;
+    let mut res2 = 0;
+    for i in 0..1000 {
+        let Reverse((d, idx)) = dists.pop().unwrap();
+        let new_net = node_networks[&idx.0];
+        let old_net = node_networks[&idx.1];
+        if new_net != old_net {
+            let [old_nodes, new_nodes] = network_nodes.get_disjoint_mut([&old_net, &new_net]);
+            let old_nodes = old_nodes.unwrap();
+            let new_nodes = new_nodes.unwrap();
+
+            for n in old_nodes.drain(..) {
+                new_nodes.push(n);
+                *node_networks.get_mut(&n).unwrap() = new_net;
+            }
+        }
+        if i == 999 {
+            let mut sorted = network_nodes.iter().map(|n| n.1.len()).collect::<Vec<_>>();
+            sorted.sort_unstable();
+            let l = sorted.len();
+            res1 = sorted[l - 1] * sorted[l - 2] * sorted[l - 3];
+        }
+    }
+
+    (res1, res2).into()
+}
--- src/aoc2025/mod.rs
+++ src/aoc2025/mod.rs
@@ -5,7 +5,17 @@ mod d04;
 mod d05;
 mod d06;
 mod d07;
+mod d08;
 
 pub fn get_funcs() -> Vec<crate::AocFun> {
-    vec![d01::f, d02::f, d03::f, d04::f, d05::f, d06::f, d07::f]
+    vec![
+        d01::f,
+        d02::f,
+        d03::f,
+        d04::f,
+        d05::f,
+        d06::f,
+        d07::f,
+        d08::f,
+    ]
 }
